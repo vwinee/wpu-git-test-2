7fdb421f155ef6152abeeea8388b93dbade39fcc
vwinee <rizkikirito86@gmail.com>
2024-08-12 17:41:37 +0900

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote</title>
+</head>
+<body>
+    <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quis fugiat quibusdam architecto, repellendus ab quia provident vitae aperiam iusto, optio accusantium exercitationem minima explicabo eaque dolor autem inventore cupiditate!</p>
+</body>
+</html>
