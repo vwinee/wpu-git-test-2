f4ae4baa35059f6b04184c078466bd75500a4ffd
vwinee <rizkikirito86@gmail.com>
2024-08-12 17:48:40 +0900

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos quis fugiat quibusdam architecto, repellendus ab quia provident vitae aperiam iusto, optio accusantium exercitationem minima explicabo eaque dolor autem inventore cupiditate!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
